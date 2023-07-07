29a6486fd46bd844cd4773a33db87bf5ec9e0e27
hodangkhoa1 <hodangkhoa110702@gmail.com>
2023-07-08 01:33:07 +0700

    Add Layout Certification

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -5,6 +5,7 @@ import Style from './home.module.scss';
 import { Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import { Carousel } from 'antd';
 
 export default function HomePage() {
   // const { translate: translateHome } = useI18n('common');
@@ -41,6 +42,67 @@ export default function HomePage() {
           />
         </SwiperSlide>
       </Swiper>
+      <div className={Style.carouselProduct}>
+        <div className={Style.productBrandName}>
+          <h2>Our Certifications</h2>
+          <h2>Our Certifications</h2>
+        </div>
+
+        <Carousel
+          slidesToShow={4}
+          className={Style.certificationCarousel}
+          autoplay
+        >
+          <div className={Style.certificationCard}>
+            <div className={Style.certificationCardImage}>
+              <img src="images/certifications/HALAL.png" alt="" />
+            </div>
+            <h1>HALAL</h1>
+          </div>
+          <div className={Style.certificationCard}>
+            <div className={Style.certificationCardImage}>
+              <img src="images/certifications/BSCI.png" alt="" />
+            </div>
+            <h1>BSCI</h1>
+          </div>
+          <div className={Style.certificationCard}>
+            <div className={Style.certificationCardImage}>
+              <img src="images/certifications/FDA.png" alt="" />
+            </div>
+            <h1>FDA</h1>
+          </div>
+          <div className={Style.certificationCard}>
+            <div className={Style.certificationCardImage}>
+              <img src="images/certifications/FSSC.png" alt="" />
+            </div>
+            <h1>FSSC22000</h1>
+          </div>
+          <div className={Style.certificationCard}>
+            <div className={Style.certificationCardImage}>
+              <img src="images/certifications/HALAL.png" alt="" />
+            </div>
+            <h1>HALAL</h1>
+          </div>
+          <div className={Style.certificationCard}>
+            <div className={Style.certificationCardImage}>
+              <img src="images/certifications/BSCI.png" alt="" />
+            </div>
+            <h1>BSCI</h1>
+          </div>
+          <div className={Style.certificationCard}>
+            <div className={Style.certificationCardImage}>
+              <img src="images/certifications/FDA.png" alt="" />
+            </div>
+            <h1>FDA</h1>
+          </div>
+          <div className={Style.certificationCard}>
+            <div className={Style.certificationCardImage}>
+              <img src="images/certifications/FSSC.png" alt="" />
+            </div>
+            <h1>FSSC22000</h1>
+          </div>
+        </Carousel>
+      </div>
     </div>
   );
 }
